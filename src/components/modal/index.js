1fddf9d26d6d4b99f65d5547c816bc42da5dde7c
Lucas Costa <lucas.garcia@auaha.com.br>
2020-04-03 19:07:54 -0300

    home 80%

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,1 +1,30 @@
 import './style';
+
+$('.navigator li').on('click', function () {
+    $('.navigator li').removeClass("active")
+    $(this).addClass("active")
+})
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+function getScrollParent(node) {
+    if (node == null) {
+        return null;
+    }
+
+    if (node.scrollHeight > node.clientHeight) {
+        return node;
+    } else {
+        return getScrollParent(node.parentNode);
+    }
+}
+
+getScrollParent()
